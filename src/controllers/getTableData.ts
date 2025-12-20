78a344558886baddc71d0ca4fd8150f1af147a4a
Ram Krishna Yadav <itsramky234@gmail.com>
2025-12-20 20:37:28 +0545

    added member funcions for fetching table data

---
+++ src/controllers/getTableData.ts
@@ -0,0 +1,43 @@
+import { Context } from "elysia";
+import SSHClient from "../SSHClient/SSHClient";
+import { PostgresSSHHelper } from "../HelperClasses/PostgresHelper";
+import { StatusCode } from "../types/types";
+import verifyMachine from "../utils/verifyMachine";
+import decryptVpsPassword from "../utils/decryptPassword";
+
+const getTableData=async({set,body,params,userId}:Context | any)=>{
+    const req=body as {databaseName:string,vpsId:string,vpsIp:string,tableName:string};
+    const {databaseName,vpsId,vpsIp,tableName}=req;
+    if(!databaseName || !vpsId || !vpsIp || !tableName){
+        set.status=StatusCode.BAD_REQUEST
+        return{
+            message:"Invalid request body."
+        }
+    }
+    var ssh:SSHClient | null =null;
+    var pgHelper:PostgresSSHHelper | null =null;
+    try {
+        const isMachineVerified=await verifyMachine(vpsId,userId,vpsIp)
+        if(!isMachineVerified.status){
+            set.status=StatusCode.NOT_FOUND
+            return{
+                message:"Invalid Machine details or you are unauthorized to access this machine"
+            }
+        }
+        const machine=isMachineVerified.machine as any;
+        const hashedPassword=await decryptVpsPassword(machine.vps_password)
+        ssh = new SSHClient({
+            host: machine.vps_ip,
+            username: "root",
+            password: hashedPassword
+        })
+        await ssh.connect();
+        console.log("connected to ssh")
+        pgHelper = new PostgresSSHHelper(ssh);
+
+    } catch (error) {
+        
+    }finally{
+
+    }
+}
